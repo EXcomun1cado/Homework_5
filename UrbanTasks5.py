82fc2107f9d767c56e3e4c7bb7080a296470bad2
EXcomun1cado <s.ismoil0818@gmail.com>
2024-07-29 14:35:33 +0300

    Add files via upload

---
+++ UrbanTasks5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 1.5, 'egg', True
+print(immutable_var)
+#immutable_var[0] = 8
+#Tuple - это неизменяемый тип данных
+
+mutable_list = ['Python', 'Java', 'C++']
+mutable_list[1] = 'C#'
+print(mutable_list)
